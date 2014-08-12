7809789c5bfc9774681f3146e0af50aa3e3c7e15
Richard Diamond <wichard@vitalitystudios.com>
2014-08-12 09:41:15 -0500

    Purge add && sub constant expression handling in -flatten-globals.

--- lib/Transforms/NaCl/FlattenGlobals.cpp
+++ lib/Transforms/NaCl/FlattenGlobals.cpp
@@ -136,10 +136,6 @@ static void ExpandConstant(DataLayout *DL, Constant *Val,
       SmallVector<Value *, 8> Indexes(CE->op_begin() + 1, CE->op_end());
       *ResultOffset += DL->getIndexedOffset(CE->getOperand(0)->getType(),
                                             Indexes);
-    } else if (CE->getOpcode() == Instruction::Add && isa<ConstantInt>(CE->getOperand(1))) {
-      *ResultOffset += cast<ConstantInt>(CE->getOperand(1))->getSExtValue();
-    } else if (CE->getOpcode() == Instruction::Sub && isa<ConstantInt>(CE->getOperand(1))) {
-      *ResultOffset -= cast<ConstantInt>(CE->getOperand(1))->getSExtValue();
     } else if (CE->getOpcode() == Instruction::BitCast ||
                CE->getOpcode() == Instruction::IntToPtr) {
       // Nothing more to do.
